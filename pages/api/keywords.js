a86349a7bfd6c5e6004dda65c36b7e9284a24f1a
Amine Aouragh <amine.aouragh.20@gmail.com>
2024-08-16 19:37:34 +0100

    add emojis to podcast keywords

---
+++ pages/api/keywords.js
@@ -0,0 +1,18 @@
+export const PODCAST_KEYWORDS = [
+    "📊 Entrepreneurship",
+    "🤖 Technology",
+    "💄 Beauty",
+    "🤡 Comedy",
+    "🌎 Society and Culture",
+    "🧠 Science",
+    "⚽ Sport",
+    "🕵 True Crime",
+    "🎨 Design",
+    "💵 Economics",
+    "🛍 Fashion",
+    "🎬 Films/Movies and TV",
+    "🥪 Food",
+    "🎮 Gaming",
+    "⚔ History",
+    "📰 News"
+]
