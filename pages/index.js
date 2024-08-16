7c965191480ccddbd43f95ea230d5118ae36fcc1
Amine Aouragh <amine.aouragh.20@gmail.com>
2024-08-16 19:36:56 +0100

    add emojis to podcast keywords

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import { PODCAST_KEYWORDS } from "./api/keywords"
 export default function Home() {
   return (
     <div className="flex flex-col justify-center absolute items-center h-full w-full p-4">
-      <h2 className="text-2xl mb-4">Podragon</h2>
-      <h1 className="text-6xl font-bold">Discover and Elevate your Podcast Journey</h1>
-      <div className="mt-16 w-1/3 flex flex-wrap justify-center">
+      <h2 className="text-2xl mb-4 text-center">🎙 Podragon 🎙</h2>
+      <h1 className="text-6xl font-bold text-center">Discover and Elevate your Podcast Journey</h1>
+      <h3 className="font-semibold text-lg text-gray-600 mt-10">Never showing you podcasts you have no interest in. Your Preferences. Your Choices. Your Rules.</h3>
+      <div className="mt-16 w-1/2 flex flex-wrap justify-center">
       {
         PODCAST_KEYWORDS.map(
           keyword => 
-          <button type="button" className="border-2 m-2 font-bold hover:bg-gray-900 hover:text-gray-50 border-gray-800 rounded-md shadow-lg px-3 py-2" key={keyword}>
+          <button type="button" className="border-2 m-2 text-2xl font-bold hover:bg-gray-900 hover:text-gray-50 border-gray-800 rounded-3xl shadow-lg px-4 py-3" key={keyword}>
             {keyword}
           </button>
         )
