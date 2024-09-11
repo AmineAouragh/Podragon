52ea342973459663bb5ca288b3e2d77ea35661c6
Amine Aouragh <amine.aouragh.20@gmail.com>
2024-09-11 20:23:01 +0100

    add function to handle podcast data insertion to postgresql table

--- backend/db.js
+++ backend/db.js
@@ -9,4 +9,29 @@ const pool = new Pool({
     port: process.env.NEXT_PUBLIC_DB_PORT
 })
 
-module.exports = pool
+async function insertPodcasts(podcasts){
+    const query = `
+     INSERT INTO podcasts (title, description, spotify_url, image_url, podcast_id, publisher)
+     VALUES ($1, $2, $3, $4, $5, $6);
+    `
+
+    for (const podcast of podcasts){
+        const values = [
+            podcast.name,
+            podcast.description,
+            podcast.external_urls.spotify,
+            podcast.images[0].url || '',
+            podcast.id,
+            podcast.publisher
+        ]
+
+        try {
+            await pool.query(query, values)
+        } catch (error) {
+            console.error("Error inserting podcasts: ", error)
+        }
+    }
+
+}
+
+module.exports = { insertPodcasts }
